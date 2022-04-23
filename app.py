8bd273704748b83fc4e2ebfd191613c76189637d
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-04-23 12:26:55 +0200

    Stage 2/4: GET vs POST

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 import sys
-
+import datetime
 from flask import Flask, jsonify
+from flask_restful import Api, reqparse, inputs, Resource
+
+
+def validate_time(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%Y-%m-%d')
+    except ValueError:
+        return False
+    return True
+
 
 app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('event', type=str, help='The event name is required!', required=True)
+parser.add_argument('date', type=inputs.date,
+                    help='The event date with the correct format is required! The correct format is YYYY-MM-DD!',
+                    required=True)
 
 
-# write your code here
+class Event(Resource):
+    def post(self):
+        args = parser.parse_args(strict=True)
+        date_event = args['date']
+        event = args['event']
+        return jsonify(
+            {
+                "message": "The event has been added!",
+                "event": event,
+                "date": date_event.strftime('%Y-%m-%d')
+            })
 
-@app.route('/event/today')
-def today_event():
-    return jsonify({"data": "There are no events for today!"})
 
+api.add_resource(Event, '/event')
 
 # do not change the way you run the program
 if __name__ == '__main__':
