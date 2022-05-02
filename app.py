f382ac0988d464bc243d957452857af1f28130a4
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-05-02 10:11:41 +0200

    Stage 3/4: Relax

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 import sys
-import datetime
 from flask import Flask, jsonify
+from datetime import date
+from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, reqparse, inputs, Resource
-
-
-def validate_time(date_text):
-    try:
-        datetime.datetime.strptime(date_text, '%Y-%m-%d')
-    except ValueError:
-        return False
-    return True
-
+from marshmallow import Schema, fields
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///calendar.db'
 api = Api(app)
+
+
+class EventSchema(Schema):
+    id = fields.Int()
+    event = fields.Str()
+    date = fields.Date()
+
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    event = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+
+db.create_all()
 
 parser = reqparse.RequestParser()
 parser.add_argument('event', type=str, help='The event name is required!', required=True)
@@ -22,20 +33,38 @@ parser.add_argument('date', type=inputs.date,
                     required=True)
 
 
-class Event(Resource):
+class EventResource(Resource):
+    def get(self):
+        schema = EventSchema(many=True)
+        events = Event.query.all()
+
+        return schema.dump(events)
+
     def post(self):
         args = parser.parse_args(strict=True)
-        date_event = args['date']
-        event = args['event']
+        date_to_add = args['date']
+        event_to_add = args['event']
+        db.session.add(Event(event=event_to_add, date=date_to_add))
+        db.session.commit()
+
         return jsonify(
             {
                 "message": "The event has been added!",
-                "event": event,
-                "date": date_event.strftime('%Y-%m-%d')
+                "event": event_to_add,
+                "date": date_to_add.strftime('%Y-%m-%d')
             })
 
 
-api.add_resource(Event, '/event')
+api.add_resource(EventResource, '/event')
+
+
+@app.route('/event/today')
+def today():
+    schema = EventSchema(many=True)
+    events = Event.query.filter(Event.date == date.today())
+    print(schema.dump(events))
+    return jsonify(schema.dump(events))
+
 
 # do not change the way you run the program
 if __name__ == '__main__':
